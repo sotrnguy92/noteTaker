b819403e6af6cca4fb94c26c0f98e1a1829acd8d
sotrnguy92 <sonnguyen092@gmail.com>
2020-12-07 22:58:56 -0800

    create route for posting a new note and adding to the db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,20 @@ const router = require("express").Router();
 const fs = require("fs");
 
 router.get('/notes', (req, res) => {
-    const db = fs.readFileSync('db/db.json');
-    res.send(JSON.parse(db))
+    const dbRead = JSON.parse(fs.readFileSync('db/db.json'));
+    res.send(dbRead)
 });
 
+router.post("/notes", (req, res) => {
+    const {title, text} = req.body;
+    const dbRead = JSON.parse(fs.readFileSync('db/db.json'));
+    const newNoteId = dbRead.length+1;
+    const noteToAdd = {title, text, id: newNoteId};
+
+    dbRead.push(noteToAdd);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbRead));
+
+    res.send(dbRead);
+});
 
 module.exports = router;
