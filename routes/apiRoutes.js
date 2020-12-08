2c59926a0a8c5b0fa68bfb63ef834950f88235bd
sotrnguy92 <sonnguyen092@gmail.com>
2020-12-07 21:22:01 -0800

    creating route to get notes from db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
+const fs = require("fs");
 
-
+router.get('/notes', (req, res) => {
+    const db = fs.readFileSync('db/db.json');
+    res.send(JSON.parse(db))
+});
 
 
 module.exports = router;
